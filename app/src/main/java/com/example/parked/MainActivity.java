8112b82aaf88b3bf3cea1fef754404c28f27fead
Ahmed Salman <amdsalman04@gmail.com>
2023-08-07 12:44:31 -0700

    Cleaned up googleMapsTutorial code. Only init map, permission and simple button

--- app/src/main/java/com/example/parked/MainActivity.java
+++ app/src/main/java/com/example/parked/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.parked;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.Button;
@@ -15,11 +19,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
@@ -29,7 +36,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
 //FIELDS----------------------------------------------------------------------------------------------
-    private Map map;
+    private GoogleMap map;
     private Button btnSetParked;
 //PLACES VARIABLES
     private Places Places;
@@ -41,6 +48,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private ActivityCompat ActivityCompat;
     private boolean locationPermissionGranted;
     private int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+    private Location lastKnownLocation;
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
 //ONCREATE----------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +75,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         btnSetParked = findViewById(R.id.btnSetParked);
-      //  btnSetParked.setOnClickListener(this);
     }
 //BUTTON METHODS----------------------------------------------------------------------------------------------
     public void setParked(){
@@ -76,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
     }
-//PLACES METHODS----------------------------------------------------------------------------------------------
+//PERMISSION METHODS----------------------------------------------------------------------------------------------
     private void getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -88,12 +97,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
+
 //MAP METHODS----------------------------------------------------------------------------------------------
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
                 .title("Marker"));
+
+        // Turn on the My Location layer and the related control on the map.
+//        updateLocationUI();
+
+       // Get the current location of the device and set the position of the map.
+//        getDeviceLocation();
     }
 
 //UNUSED----------------------------------------------------------------------------------------------
